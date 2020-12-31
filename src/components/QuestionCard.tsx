d0dfd5fd3b6376034b7136bf8239e4bb6af4ab06
Hanna Kim <hannakim91@gmail.com>
2020-12-31 12:50:38 -0700

    Add prop types to QuestionCard

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
+
+type Props = {
+  question: string;
+  answer: string[];
+  callback: any;
+  userAnswer: string;
+  questionNum: number;
+  totalQuestions: number;
+}
+
 const QuestionCard = () => 
 <div>
   Question Card
