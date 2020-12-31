9390d7d721fa9bed1a41fb45fff478323c63d85b
Hanna Kim <hannakim91@gmail.com>
2020-12-31 12:46:57 -0700

    Fix bug: change declaration of App from function to const

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function App = () => {
+const App = () => {
 
   const startTrivia = async () => {
 
@@ -11,12 +11,18 @@ function App = () => {
   }
 
   const nextQuestion = () => {
-    
+
   }
 
   return (
     <div className="App">
-      Quiz
+      <h1>REACT QUIZ</h1>
+      <button className="start" onClick={startTrivia}>
+      Start
+      </button>
+      <p className="score">Score:</p>
+      <p>Loading Questions...</p>
+
     </div>
   );
 }
