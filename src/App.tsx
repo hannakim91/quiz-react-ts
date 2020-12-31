20108bc3924390f5ae7db7210ce55151a5849ee1
Hanna Kim <hannakim91@gmail.com>
2020-12-31 12:43:26 -0700

    Scaffolding for startTrivia and checkAnswer methods for App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 
 function App = () => {
+
+  const startTrivia = async () => {
+
+  }
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
   return (
     <div className="App">
       Quiz
