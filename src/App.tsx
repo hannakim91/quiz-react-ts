af5c7a0fb20716c7c2ee49d70f935343403385b4
Hanna Kim <hannakim91@gmail.com>
2020-12-31 13:35:51 -0700

    Add conditional displays for score and loading elements

--- src/App.tsx
+++ src/App.tsx
@@ -53,8 +53,8 @@ const App = () => {
         Start
         </button>
       ) : null}
-      <p className="score">Score:</p>
-      <p>Loading Questions...</p>
+      {!gameOver ? <p className="score">Score:</p> : null}
+      {loading && <p>Loading Questions...</p>}
       {/* <QuestionCard
         questionNum={number + 1}
         totalQuestions={TOTAL_QUESTIONS}
