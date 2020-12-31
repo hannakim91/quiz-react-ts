f8f419a8a35e4dce3c967f515f511aacfe88b492
Hanna Kim <hannakim91@gmail.com>
2020-12-31 13:20:53 -0700

    Create type: QuestionState

--- src/API.ts
+++ src/API.ts
@@ -2,7 +2,7 @@ export enum Difficulty {
   EASY = "easy",
   MEDIUM = "medium",
   HARD = "hard"
-}
+};
 
 export type Question = {
   category: string;
@@ -11,13 +11,15 @@ export type Question = {
   incorrect_answer: string[];
   question: string;
   type: string;
-}
+};
+
+export type QuestionState = Question & { answers: string[] };
 
 export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
   const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
   const data = await (await fetch(endpoint)).json();
   console.log(data);
-}
+};
 
 // why double await?
   // first await fetch itself
